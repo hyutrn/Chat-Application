fd7afdb9e76cf5527924d03e2cb5706774bf1cb9
TrongHuy <115525478+hyutrn@users.noreply.github.com>
2024-05-27 15:55:22 +0700

    upload code

---
+++ server.cpp
@@ -0,0 +1,203 @@
+#include "serverSocket.h"
+#include <unordered_map>
+
+std::vector<Account> accounts;
+std::vector<SOCKET> clients;
+std::unordered_map<std::string, bool> loggedInAccounts;
+std::mutex clientsMutex;
+std::mutex accountsMutex;
+
+// Hàm tạo account khi nhận được key = 0 từ chuỗi tin nhắn
+void CreateAccount(const std::string& username, const std::string& password) {
+    // Thêm username, password vào vector accounts
+    accounts.push_back({username, password});
+    std::cout << "Account created: " << username << std::endl;
+
+}
+
+bool CheckAccount(const std::string& username, const std::string& password) {
+    std::lock_guard<std::mutex> lock(accountsMutex);
+    for (const auto& account : accounts) {
+        if (account.username == username && account.password == password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void LogoutAccount(const std::string& username) {
+    std::lock_guard<std::mutex> lock(accountsMutex);
+    loggedInAccounts[username] = false;
+}
+
+void BroadcastMessage(const std::string& message, SOCKET senderSocket) {
+    std::lock_guard<std::mutex> lock(clientsMutex);
+    for (SOCKET client : clients) {
+        if (client != senderSocket) {
+            send(client, message.c_str(), message.length(), 0);
+        }
+    }
+}
+
+void HandleClient(SOCKET clientSocket) {
+    char buffer[1024] = {0};
+    int recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (recvSize == SOCKET_ERROR) {
+        std::cout << "Receive failed" << std::endl;
+        closesocket(clientSocket);
+        return;
+    }
+
+    std::string message(buffer, recvSize);
+    size_t pos1 = message.find(',');
+    size_t pos2 = message.rfind(',');
+
+    if (pos1 == std::string::npos || pos2 == std::string::npos || pos1 == pos2) {
+        std::cout << "Invalid message format" << std::endl;
+        closesocket(clientSocket);
+        return;
+    }
+
+    std::string username = message.substr(0, pos1);
+    std::string password = message.substr(pos1 + 1, pos2 - pos1 - 1);
+    int key = std::stoi(message.substr(pos2 + 1));
+
+    if (key == 0) {
+        CreateAccount(username, password);
+        // Tạo account thành công, gửi mã 200 về client
+        send(clientSocket, "200", 3, 0);
+        while(recv(clientSocket, buffer, sizeof(buffer), 0)){
+            std::string message(buffer, recvSize);
+            size_t pos1 = message.find(',');
+            size_t pos2 = message.rfind(',');
+            if (pos1 == std::string::npos || pos2 == std::string::npos || pos1 == pos2) {
+                std::cout << "Invalid message format" << std::endl;
+                closesocket(clientSocket);
+                return;
+            }
+            std::string username = message.substr(0, pos1);
+            std::string password = message.substr(pos1 + 1, pos2 - pos1 - 1);
+
+            // Kiểm tra account có trong hệ thống và không được đăng nhập, gửi mã 201 về client
+            if (CheckAccount(username, password)) {
+                std::cout << "Account login: " << username << std::endl;
+                send(clientSocket, "201", 3, 0);
+                break;
+            } else {
+                std::cout << "Account login failed: " << username << std::endl;
+                send(clientSocket, "401", 3, 0);
+                closesocket(clientSocket);
+                return;
+            }
+        }
+    } else if (key == 1) {
+        // Kiểm tra account có trong hệ thống và không được đăng nhập, gửi mã 201 về client
+        if (CheckAccount(username, password)) {
+            std::cout << "Account login: " << username << std::endl;
+            send(clientSocket, "201", 3, 0);
+        } else {
+            std::cout << "Account login failed: " << username << std::endl;
+            send(clientSocket, "401", 3, 0);
+            closesocket(clientSocket);
+            return;
+        }
+    } else {
+        std::cout << "Invalid key value" << std::endl;
+        closesocket(clientSocket);
+        return;
+    }
+
+    {
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        clients.push_back(clientSocket);
+    }
+
+    while (true) {
+        recvSize = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (recvSize > 0) {
+            std::string clientMessage(buffer, recvSize);
+            if (clientMessage == "./exit") {
+                std::cout << "Account disconnected: " << username << std::endl;
+                break;
+            } else {
+                BroadcastMessage(clientMessage, clientSocket);
+            }
+        } else {
+            // Lock the mutex before erasing from clients vector
+            {
+                std::lock_guard<std::mutex> lock(clientsMutex);
+                auto it = std::remove_if(clients.begin(), clients.end(), [clientSocket](SOCKET s) {
+                    return s == clientSocket;
+                });
+                clients.erase(it, clients.end());
+            }
+
+            closesocket(clientSocket);
+            break;
+        }
+    }
+
+    // Remove the client from the clients list and close the socket
+    {
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        clients.erase(std::remove(clients.begin(), clients.end(), clientSocket), clients.end());
+    }
+
+    LogoutAccount(username); // Đánh dấu tài khoản là đã đăng xuất
+    closesocket(clientSocket);
+}
+
+int StartServer() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cout << "Socket creation failed" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    // serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_addr.s_addr = inet_addr(IP_SERVER);
+    serverAddr.sin_port = htons(8080);
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cout << "Bind failed" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cout << "Listen failed" << std::endl;
+        closesocket(serverSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server is listening on port 8080..." << std::endl;
+
+    while (true) {
+        SOCKET clientSocket = accept(serverSocket, nullptr, nullptr);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cout << "Accept failed" << std::endl;
+            closesocket(serverSocket);
+            WSACleanup();
+            return 1;
+        }
+
+        std::thread clientThread(HandleClient, clientSocket);
+        clientThread.detach();
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
+
+int main() {
+    return StartServer();
+}
