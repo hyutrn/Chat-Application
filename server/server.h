93caf17cc202c65713eeb4a84c2357b6dd2fc649
Trong Huy <115525478+hyutrn@users.noreply.github.com>
2024-06-25 09:19:06 +0700

    Final update

--- server/server.h
+++ server/server.h
@@ -13,14 +13,18 @@
 #include<fstream>
 #include <cstdlib> // thư viện cần thiết cho rand() và srand()
 #include <ctime>   // thư viện cần thiết cho time()
+#include <set>
+#include <bitset>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 #define PORT_SERVER 8080
-#define IP_SERVER "192.168.37.44"
 
 #define FILE_NAME "account.txt"
 std::string keyEcrypt = "hello";
+std::string keyFile = "123";
+
+std::string ipServer = "192.168.37.110";
 
 struct Account {
     std::string username;
@@ -28,16 +32,55 @@ struct Account {
     int key;
 };
 
+struct Message
+{
+    std::string roomName;
+    std::string userName;
+    std::string message;
+};
+std::set<std::string> roomAvailable;
+std::set<std::string> roomPrivateAllocate;
+
+// class ChatRoom {
+// public:
+//     std::string roomName;
+//     std::string roomKey;
+//     ChatRoom(const std::string& name, const std::string& key) : roomName(name), roomKey(key) {}
+// };
+std::unordered_map<std::string, std::string> roomPrivate;
+
+// List of user
+void listofUser(const std::string&message, SOCKET senderSocket, const std::string& username);
+
+// Enscrypt and descrypt message
 std::string encryptMessage(const std::string& plaintext, const std::string& key);
+std::string decryptMessage(const std::string& ciphertext, const std::string& key);
+
+// Generate, check ID for user
 bool CheckID(const std::string& ID, const std::string& fileName);
 int generateID();
+
+// Handle login, sign up and log out
 void logAccount(const std::string& username, int flagAccount);
 int insertAccount(const std::string& userName, const std::string& passWord, const int ID, const std::string& fileName, SOCKET clientSocket);
 int CreateAccount(const std::string& username, const std::string& password);
 bool CheckAccount(const std::string& userName, const std::string password,  const std::string& fileName);
+bool CheckLogIn(const std::string& username, const std::string& password, const std::string& fileName);
 Account CheckMessage(const std::string& message);
+std::string generateKey();
+// Broadcast message
 void BroadcastMessage(const std::string& message, SOCKET senderSocket, const std::string& username);
-void HandleClient(SOCKET clientSocket);
-int StartServer();
 
+/*
+// Read, write log chat in room
+void writeChat();
+void loadChat();
+*/
+
+// Handl client
+void HandleClient(SOCKET clientSocket);
+
+// Config server
+int StartServer();
+//ha
 #endif // SERVER_H
